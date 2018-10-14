9740349fb20d176a0bbd663d46cc7784e9f9f610
sadoneli <sadoneli@gmail.com>
2018-10-14 21:13:06 +0800

    update message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>软件中心&nbsp;-&nbsp;by&nbsp;koolshare</em>",
 "content1":"1.3.6更新日志：修复未安装里出现图标出走的问题。",
-"content2":"好消息：2018年10月2日软件中心新增【迅雷快鸟】插件！",
+"content2":"好消息：2018年10月14日软件中心新增【单线多拨】插件！",
 "content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/koolshare/rogsoft' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/xbchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
