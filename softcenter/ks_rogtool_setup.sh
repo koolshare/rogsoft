2079ef1ec86e8797db34adcb24a3c740506f0456
sadoneli <sadoneli@gmail.com>
2020-09-04 20:29:26 +0800

    update again

--- softcenter/ks_rogtool_setup.sh
+++ softcenter/ks_rogtool_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /tmp
-rm -rf /jffs/ks_rogtool_setup
+rm -rf /tmp/ks_rogtool_setup
 wget --no-check-certificate https://rogsoft.ddnsto.com/softcenter/ks_rogtool_setup
-chmod +x ks_rogtool_setup
-/tmp/ks_rogtool_setup install
-rm -rf /tmp/ks_rogtool_setup*
+chmod +x ./ks_rogtool_setup
+./ks_rogtool_setup install
+rm -rf /tmp/ks_rogtool_setup
