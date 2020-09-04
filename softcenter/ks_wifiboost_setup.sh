5935ae7f8a86709629356e0ee1fcfd3622340075
sadoneli <sadoneli@gmail.com>
2020-09-04 15:13:04 +0800

    update ks_wifiboost_setup.sh script

--- softcenter/ks_wifiboost_setup.sh
+++ softcenter/ks_wifiboost_setup.sh
@@ -5,5 +5,4 @@ rm -rf /jffs/ks_wifiboost_setup
 wget --no-check-certificate https://rogsoft.ddnsto.com/softcenter/ks_wifiboost_setup
 chmod +x ks_wifiboost_setup
 /tmp/ks_wifiboost_setup install
-cp -rf /tmp/ks_wifiboost_setup /koolshare/bin
 rm -rf /tmp/ks_wifiboost_setup*
