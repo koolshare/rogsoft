d584daf8321791ff3a444b5004075639d8fc9425
sadoneli <sadoneli@gmail.com>
2020-09-17 15:31:53 +0800

    update wifiboost

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -254,15 +254,15 @@
             "version": "1.6"
         }, 
         {
-            "build_date": "2020-09-17_10:40:59", 
+            "build_date": "2020-09-17_15:31:25", 
             "changelog": "", 
             "description": "wifi boost 路由器功率增强，强过澳大利亚", 
             "home_url": "Module_wifiboost.asp", 
-            "md5": "1563bad88c0291f7098b5e613af3faf9", 
+            "md5": "b4256d055fe8596436d61c3ec1bc46f0", 
             "name": "wifiboost", 
             "tar_url": "wifiboost/wifiboost.tar.gz", 
             "title": "wifi boost", 
-            "version": "2.6"
+            "version": "2.7"
         }
     ], 
     "home_url": "https://raw.githubusercontent.com/koolshare/koolshare.github.io/master", 
--- wifiboost/config.json.js
+++ wifiboost/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"2.6",
-"md5":"1563bad88c0291f7098b5e613af3faf9",
+"version":"2.7",
+"md5":"b4256d055fe8596436d61c3ec1bc46f0",
 "home_url":"Module_wifiboost.asp",
 "title":"wifi boost",
 "description":"wifi boost 路由器功率增强，强过澳大利亚",
 "changelog":"",
-"build_date":"2020-09-17_10:40:59"
+"build_date":"2020-09-17_15:31:25"
 }
