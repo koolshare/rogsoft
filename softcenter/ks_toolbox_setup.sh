d2dc6860c26189c9af4421eb035eae5c85562a46
sadoneli <sadoneli@gmail.com>
2020-09-21 20:49:49 +0800

    update

--- softcenter/ks_toolbox_setup.sh
+++ softcenter/ks_toolbox_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cd /tmp
-rm -rf /tmp/ks_wifiboost_setup
 wget --no-check-certificate https://rogsoft.ddnsto.com/softcenter/ks_wifiboost_setup
 chmod +x ./ks_wifiboost_setup
 ./ks_wifiboost_setup install
@@ -10,7 +9,6 @@ rm -rf /tmp/ks_wifiboost_setup
 echo
 
 cd /tmp
-rm -rf /tmp/ks_rogtool_setup
 wget --no-check-certificate https://rogsoft.ddnsto.com/softcenter/ks_rogtool_setup
 chmod +x ./ks_rogtool_setup
 ./ks_rogtool_setup install
