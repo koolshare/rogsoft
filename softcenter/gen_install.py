b15662a6374cda886975e49813066b8fc655a6d1
sadoneli <sadoneli@gmail.com>
2020-09-30 14:09:44 +0800

    temporary block frps

--- softcenter/gen_install.py
+++ softcenter/gen_install.py
@@ -31,7 +31,7 @@ def work_paths_by_walk():
             print os.path.join(root,sub)
 
 def work_parent():
-    ignore_paths = frozenset(["maintain_files", "softcenter", "appledns", "speedtest", "dualwan", "kcptun", "koolnet", "kuainiao", "shadowvpn", "tunnel", "ssid", "ssserver", "cfetool"])
+    ignore_paths = frozenset(["maintain_files", "softcenter", "appledns", "speedtest", "dualwan", "kcptun", "koolnet", "kuainiao", "shadowvpn", "tunnel", "ssid", "ssserver", "cfetool", "frps"])
     fnames = os.listdir(parent_path)
     fnames.sort()
     for fname in fnames:
