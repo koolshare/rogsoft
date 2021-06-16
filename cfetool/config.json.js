546b3a5f38c4c25fbc4a31984dd602953f4f7eb7
sadoneli <sadoneli@gmail.com>
2021-06-16 20:29:09 +0800

    add 2.5G PHY detect for RT-AX86U

--- cfetool/config.json.js
+++ cfetool/config.json.js
@@ -1,11 +1,11 @@
 {
-"version":"3.0",
-"md5":"e5fbaf22927119fd84a9a34fe7e06628",
+"version":"3.1",
+"md5":"49b52ebe4f5b3abb2ca56daca9101249",
 "home_url":"Module_cfetool.asp",
 "title":"CFE工具箱",
 "description":"CFE工具箱，查看CFE信息，改机器为国区",
 "changelog":"",
-"build_date":"2021-05-07_12:57:19",
+"build_date":"2021-06-16_20:28:15",
 "server":"42.192.18.234",
 "port":"8083"
 }
