2547a09507a3f56c03e41b808b99ae31cd2b33b5
sadog <sadoneli@gmail.com>
2021-08-13 14:46:11 +0800

    Update ks_home_status.sh

--- softcenter/softcenter/scripts/ks_home_status.sh
+++ softcenter/softcenter/scripts/ks_home_status.sh
@@ -4,47 +4,59 @@ export KSROOT=/koolshare
 source $KSROOT/scripts/base.sh
 
 _get_model(){
-	local odmpid=$(nvram get odmpid)
-	local MODEL=$(nvram get productid)
-	local _tmp="unknow"
-	if [ -n "${odmpid}" ];then
-		_tmp=$(echo "${odmpid}"|tr '[A-Z]-' '[a-z]_')
-	else
-		_tmp=$(echo "${MODEL}"|tr '[A-Z]-' '[a-z]_')
-	fi
-	echo ${_tmp}
+  local odmpid=$(nvram get odmpid)
+  local MODEL=$(nvram get productid)
+  local _tmp="unknow"
+  if [ -n "${odmpid}" ];then
+    _tmp=$(echo "${odmpid}"|tr '[A-Z]-' '[a-z]_')
+  else
+    _tmp=$(echo "${MODEL}"|tr '[A-Z]-' '[a-z]_')
+  fi
+  echo ${_tmp}
 }
 
 _get_mac(){
-	local MACADDR=$(nvram get et0macaddr)||"FF:FF:FF:FF:FF:FF"
-	local _MACMD5=$(echo $MACADDR|md5sum)
- 	local _MACMD5=${_MACMD5% -}
-	echo $_MACMD5
+  local MACADDR=$(nvram get et0macaddr)||"FF:FF:FF:FF:FF:FF"
+  local _MACMD5=$(echo $MACADDR|md5sum)
+  local _MACMD5=${_MACMD5% -}
+  echo $_MACMD5
 }
 
-DDNSTO_URL=`dbus get softcenter_ddnsto_url`
+get_current_jffs_status(){
+  local cur_patition=$(df | /bin/grep /jffs)
+  if [ -z "${cur_patition}" ];then
+    local used=$(echo ${cur_patition} | awk '{print $3}')
+    local total=$(echo ${cur_patition} | awk '{print $2}')
+    echo "${used} ${total}"
+  else
+    echo "0 0"
+  fi
+}
+
+eval $(dbus export ddnsto_)
+DDNSTO_URL=${ddnsto_url}
 if [ -z "${DDNSTO_URL}" ]; then
-	UQ_ID=$(_get_mac)
-	DDNSTO_URL=$(echo "https://asus-${UQ_ID:0:8}.kooldns.cn")
+  UQ_ID=$(_get_mac)
+  DDNSTO_URL=$(echo "https://asus-${UQ_ID:0:8}.kooldns.cn")
 fi
 
-DDNSTO_INSTALL=`dbus get softcenter_module_ddnsto_install`
+DDNSTO_INSTALL=$(dbus get softcenter_module_ddnsto_install)
 if [ -n "${DDNSTO_INSTALL}" ]; then
-	if [ ! -f "/koolshare/bin/ddnsto" ]; then
-		DDNSTO_INSTALL=0
-	fi
+  if [ ! -f "/koolshare/bin/ddnsto" ]; then
+    DDNSTO_INSTALL=0
+  fi
 else
-	DDNSTO_INSTALL=0
+  DDNSTO_INSTALL=0
 fi
 
-DDNSTO_STATUS=`ps | grep -w ddnsto | grep -cv grep`
-DDNSTO_PID=`pidof ddnsto`
+DDNSTO_STATUS=$(ps | grep -w ddnsto | grep -cv grep)
+DDNSTO_PID=$(pidof ddnsto)
 
-BOOT_CFG=`nvram get ddnsto_boot_cfg`
+BOOT_CFG=$(nvram get ddnsto_boot_cfg)
 DDNSTO_DEVICE_ID=
 if [ -n "${BOOT_CFG}" ]; then
-	DDNSTO_DEVICE_ID=`/koolshare/bin/ddnsto -w|cut -d ' ' -f2`
+  DDNSTO_DEVICE_ID=$(/koolshare/bin/ddnsto -w|cut -d ' ' -f2)
 fi
 
-RESP=`echo '{\"ddnsto_url\":\"'${DDNSTO_URL}'\",\"ddnsto_install\":'${DDNSTO_INSTALL}',\"ddnsto_status\":'${DDNSTO_STATUS}',\"ddnsto_device_id\":\"'${DDNSTO_DEVICE_ID}'\",\"ddnsto_pid\":\"'${DDNSTO_PID}'\"}'`
-http_response "${RESP}"
+RESP=$(echo '{\"ddnsto_url\":\"'${DDNSTO_URL}'\",\"ddnsto_token\":\"'${ddnsto_token}'\",\"ddnsto_install\":'${DDNSTO_INSTALL}',\"ddnsto_status\":'${DDNSTO_STATUS}',\"ddnsto_device_id\":\"'${DDNSTO_DEVICE_ID}'\",\"ddnsto_pid\":\"'${DDNSTO_PID}'\",\"jffs_status\":\"'$(get_current_jffs_status)'\"}')
+http_response  "${RESP}"
