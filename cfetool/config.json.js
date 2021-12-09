1427d87b06c0b7dabd96b752535f42b89ce3c827
sadog <sadoneli@gmail.com>
2021-12-09 20:52:00 +0800

    update

--- cfetool/config.json.js
+++ cfetool/config.json.js
@@ -1,6 +1,6 @@
 {
 "version":"3.3",
-"md5":"0d90a402ea0c5cec073859c069e4b4c5",
+"md5":"17c59b16c47b6d168c14d201d4819a35",
 "home_url":"Module_cfetool.asp",
 "title":"CFE工具箱",
 "description":"CFE工具箱，查看CFE信息，改机器为国区",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2021-12-09_20:42:43",
+"build_date":"2021-12-09_20:51:50",
 "server":"42.192.18.234",
 "port":"8083"
 }
