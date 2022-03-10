290c3059cf4d24a24ddf1da22cc71aad657d9441
sadog <sadoneli@gmail.com>
2022-03-10 23:48:44 +0800

    add rax70

--- uamas/config.json.js
+++ uamas/config.json.js
@@ -1,9 +1,9 @@
 {
 "version":"1.0",
-"md5":"5a73fc62d5c471c4b88b41cb5cc94445",
+"md5":"9fdccfa86343ee81793de46f24506ff6",
 "home_url":"Module_uamas.asp",
 "title":"Unlock AiMesh",
 "description":"网件梅林固件解锁AiMesh",
 "changelog":"",
-"build_date":"2021-08-20_22:31:27",
+"build_date":"2022-03-10_23:48:30",
 }
