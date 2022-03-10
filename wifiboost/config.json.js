6f9f7f695b360c76f57282a779604ba5774336bd
sadog <sadoneli@gmail.com>
2022-03-10 23:44:03 +0800

    add rax70 support

--- wifiboost/config.json.js
+++ wifiboost/config.json.js
@@ -1,6 +1,6 @@
 {
 "version":"4.6",
-"md5":"7b098f9e750c764fb4ec0c18f6374e7e",
+"md5":"1aa8c924ed8412828f2bbe8996c7615f",
 "home_url":"Module_wifiboost.asp",
 "title":"wifi boost",
 "description":"wifi boost: 路由器WiFi功率增强，增加信号覆盖范围。",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2022-03-08_22:49:43",
+"build_date":"2022-03-10_23:32:30",
 "server":"42.192.18.234",
 "port":"8083"
 }
