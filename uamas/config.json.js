f63c2c525cb85755238069ad23d54137b1401de6
sadog <sadoneli@gmail.com>
2022-03-10 23:57:09 +0800

    fix rax70/rax80

--- uamas/config.json.js
+++ uamas/config.json.js
@@ -1,9 +1,9 @@
 {
 "version":"1.0",
-"md5":"9fdccfa86343ee81793de46f24506ff6",
+"md5":"d43e00d5ec968fd8d3cc12d75cd16a96",
 "home_url":"Module_uamas.asp",
 "title":"Unlock AiMesh",
 "description":"网件梅林固件解锁AiMesh",
 "changelog":"",
-"build_date":"2022-03-10_23:48:30",
+"build_date":"2022-03-10_23:56:54",
 }
