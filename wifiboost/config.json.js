989a38182f85b59468aa199a27bcd9511b6f03bc
sadog <sadoneli@gmail.com>
2023-03-25 00:24:12 +0800

    update wifiboost to fix status

--- wifiboost/config.json.js
+++ wifiboost/config.json.js
@@ -1,6 +1,6 @@
 {
 "version":"4.9",
-"md5":"7aa7e5ceeb201970151294626a9e1f90",
+"md5":"1f8f26468c8d12b0d15a76af474f5eee",
 "home_url":"Module_wifiboost.asp",
 "title":"wifi boost",
 "description":"wifi boost: 路由器WiFi功率增强，增加信号覆盖范围。",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2023-03-25_00:03:46",
+"build_date":"2023-03-25_00:23:57",
 "server":"42.192.18.234",
 "port":"8083"
 }
