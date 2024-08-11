758d9e7fc057ab3cc3b7ef6ca2f8b0500d0f98b7
sadog <sadoneli@gmail.com>
2024-08-12 00:24:00 +0800

    update for GT-BE96

--- wifiboost/config.json.js
+++ wifiboost/config.json.js
@@ -1,6 +1,6 @@
 {
 "version":"4.9.4",
-"md5":"26ccaf031ff187a986d97f3c3642cf4f",
+"md5":"eeeb48b3478ad0a1b63a901b1211a8d5",
 "home_url":"Module_wifiboost.asp",
 "title":"wifi boost",
 "description":"wifi boost: 路由器WiFi功率增强，增加信号覆盖范围。",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2024-07-31_09:47:44",
+"build_date":"2024-08-12_00:22:52",
 "server":"42.192.18.234",
 "port":"8083"
 }
