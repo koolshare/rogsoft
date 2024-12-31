0066aa6ee99c7ce7fd226122f029acc425b6a0da
sadog <sadoneli@gmail.com>
2024-12-31 22:02:01 +0800

    update fixtit

--- fixit/config.json.js
+++ fixit/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"1.3",
-"md5":"f0ff69a954427b5804418f27d110983d",
+"version":"1.5",
+"md5":"f58d0a22fc73d0922b2973edf172c642",
 "home_url":"Module_fixit.asp",
 "title":"安全检查",
 "description":"检测软件中心重要文件是否被篡改，如果检测到恶意篡改，将其修复",
@@ -8,5 +8,5 @@
 "author":"sadog",
 "link":"",
 "changelog":"",
-"build_date":"2024-12-31_11:51:50"
+"build_date":"2024-12-31_22:01:48"
 }
