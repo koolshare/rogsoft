39e3669bb891610d2039031c6fc190cf9804cdcb
sadog <sadoneli@gmail.com>
2025-03-14 01:39:57 +0800

    fix error3 on be88u

--- wifiboost/config.json.js
+++ wifiboost/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"4.9.6",
-"md5":"1dc2ac9f7776581bb02dcc43b7edf40f",
+"version":"4.9.7",
+"md5":"81f34c47d75f680b7d380e0dcf04c98d",
 "home_url":"Module_wifiboost.asp",
 "title":"wifi boost",
 "description":"wifi boost: 路由器WiFi功率增强，增加信号覆盖范围。",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2024-12-26_00:03:09",
+"build_date":"2025-03-14_01:38:00",
 "server":"42.192.18.234",
 "port":"8083"
 }
