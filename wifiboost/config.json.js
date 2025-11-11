04c015d62c58c9bca0fa6c9a1b249e2fb2d556a2
sadog <sadoneli@gmail.com>
2025-11-11 23:43:02 +0800

    update wifiboost to fix wifi offline

--- wifiboost/config.json.js
+++ wifiboost/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"4.9.9",
-"md5":"5d1beb4807163cff2065ccbb34865141",
+"version":"4.9.11",
+"md5":"d0e309856698a27c42e9bddf39c6e36d",
 "home_url":"Module_wifiboost.asp",
 "title":"wifi boost",
 "description":"wifi boost: 路由器WiFi功率增强，增加信号覆盖范围。",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2025-11-11_21:28:58",
+"build_date":"2025-11-11_23:41:34",
 "server":"42.192.18.234",
 "port":"8083"
 }
