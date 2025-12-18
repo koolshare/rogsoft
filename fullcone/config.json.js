a09f5b32eb28c6646fff8cad61582466dc304511
sadog <sadoneli@gmail.com>
2025-12-18 08:32:54 +0800

    fullcone 1.0.7

--- fullcone/config.json.js
+++ fullcone/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"1.0.6",
-"md5":"2e379bb6fadf92c104a6f214b56d1a23",
+"version":"1.0.7",
+"md5":"1b7efa33efd7982c65a805765cc7e045",
 "home_url":"Module_fullcone.asp",
 "title":"Fullcone NAT",
 "description":"使用此插件可以让你的网络轻松变为Full Cone（全锥形）即NAT1",
@@ -8,7 +8,7 @@
 "tags":"系统 工具",
 "author":"sadog",
 "link":"",
-"build_date":"2025-12-17_22:08:43",
+"build_date":"2025-12-18_08:30:03",
 "server":"42.192.18.234",
 "port":"8083"
 }
