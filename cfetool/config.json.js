bb7f4ef53cd530faebe8df825c9105b066e316fa
sadog <sadoneli@gmail.com>
2026-02-16 21:42:57 +0800

    add gt6 support for cfetool

--- cfetool/config.json.js
+++ cfetool/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"4.6",
-"md5":"66c7405996f61d27430e7dd381f6d5cc",
+"version":"4.7",
+"md5":"b66f840353b9a741c425611e21499677",
 "home_url":"Module_cfetool.asp",
 "title":"CFE工具箱",
 "description":"CFE工具箱，查看CFE信息，改机器为国区",
@@ -8,7 +8,7 @@
 "author":"sadog",
 "link":"",
 "changelog":"",
-"build_date":"2025-11-02_23:47:47",
+"build_date":"2026-02-16_21:42:37",
 "server":"42.192.18.234",
 "port":"8083"
 }
